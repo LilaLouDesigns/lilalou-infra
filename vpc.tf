93c2c3bdc58e8ad1d95dd0ec3c89fdaeb008005e
Nga <92612527+NgaNaNa@users.noreply.github.com>
2023-03-05 20:26:59 +1030

    creates route table association for public

--- vpc.tf
+++ vpc.tf
@@ -62,3 +62,10 @@ resource "aws_route_table" "public-rt" {
     Name = "${var.basename}-public"
   }
 }
+
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.subnet-public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public-rt.id
+}
